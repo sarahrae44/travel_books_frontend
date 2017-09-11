bf00675a3a7b9f0d107dd2ab089ea2acc7846bbc
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-10 18:32:56 -0600

    update user function

--- public/app.js
+++ public/app.js
@@ -6,6 +6,8 @@ app.controller('mainController', ['$http', function($http) {
   this.user = {};
   this.users = [];
   this.userPass = {};
+  this.token = {};
+  this.editUser = {};
 
   //hidden pages
   this.home = true;
@@ -46,7 +48,7 @@ app.controller('mainController', ['$http', function($http) {
       url: this.url + '/users/login',
       data: { user: { username: userPass.username, password: userPass.password }},
     }).then(function(response) {
-      // console.log(response);
+      console.log(response);
       this.user = response.data.user;
       localStorage.setItem('token', JSON.stringify(response.data.token));
     }.bind(this));
@@ -69,6 +71,45 @@ app.controller('mainController', ['$http', function($http) {
         this.users = response.data;
       }
     }.bind(this));
+  }
+
+  this.editUser = function() {
+    $http({
+      url: this.url + '/users',
+      method: 'PUT',
+      headers: {
+      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+      }.then(function(response) {
+      console.log(response);
+      this.user = response.data.user;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+    }.bind(this))
+    })
+  }
+
+
+  this.updateUser = function() {
+    $http({
+      url: this.url + '/users',
+      method: 'POST',
+      headers: {
+      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+      }.then(function(response) {
+      console.log(response);
+      this.user = response.data.user;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+    }.bind(this))
+    })
+  }
+
+  //delete user
+  this.deleteUser = function(userPass) {
+    console.log('trying to delete user');
+    // $http({
+    //   url: this.url + '/users',
+    //   method: ''
+    //
+    // })
   }
 
   this.logout = function() {
